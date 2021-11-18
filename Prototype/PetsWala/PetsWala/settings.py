ac881a0ca475a90e0c493241d711feddb480ba89
Aaish <22100300@lums.edu.pk>
2021-11-18 15:54:55 +0500

    django-heroku added

--- Prototype/PetsWala/PetsWala/settings.py
+++ Prototype/PetsWala/PetsWala/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,3 +147,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'blog-home'
+
+django_heroku.settings(locals())
