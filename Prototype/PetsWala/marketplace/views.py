64c39c12eb54dca7069a6d98b480717dda635321
Tayyab Hussain <22100200@lums.edu.pk>
2021-11-26 18:16:11 +0500

    Added product details page

--- Prototype/PetsWala/marketplace/views.py
+++ Prototype/PetsWala/marketplace/views.py
@@ -3,10 +3,25 @@ from .models import Product
 
 # Create your views here.
 
-def marketplace(request):
-    data = Product.objects.all()
-    mkt = {
-        "products": data
-    }
 
-    return render(request, 'marketplace/marketplace.html', mkt)
+def marketplace(request):
+    products = Product.objects.all()
+    context = {
+        'products' : products
+    }
+    return render(request,'marketplace/marketplace.html',context)
+
+def product(request, id=0):
+    product = {}
+    search = Product.objects.filter(id=id)
+    isFound = (len(search)==1)
+    if isFound:
+        product = search[0]
+        
+    products = Product.objects.exclude(id=id)[:4]
+    context = {
+        'product' : product,
+        'found' : isFound,
+        'products' : products
+    }
+    return render(request, 'marketplace/detail.html',context)
