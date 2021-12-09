f65801f8a5926ebb957e36e8c24bbe71a0fe8c76
Tayyab Hussain <22100200@lums.edu.pk>
2021-12-09 05:16:13 +0500

    Cleaned up

--- Development/Sprint 2/PetsWala/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path('register/', views.register, name='register'),
-    path('user_register/', views.user_register.as_view(), name='user_register'),
-    path('vendor_register/', views.vendor_register.as_view(), name='vendor_register'),
-    
-]
